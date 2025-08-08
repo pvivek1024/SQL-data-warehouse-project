e5528ad2d86dedd6a373f65a9af6a61cedaf4b38
pvivek1024 <prakashvivek05@gmail.com>
2025-08-08 18:30:25 +0530

    Update int_database.sql

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -1,13 +1,25 @@
---CREATE DATABASE DATAWAREHOUSE--
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'.
+    The script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+	*/
 
 use master;
+go
 
+--CREATE DATABASE DATAWAREHOUSE--
 create database datawarehouse;
-
+go
+--create Schemas  
 use Datawarehouse;
-
+go
+  
 create schema bronze;
 go
 create schema silver;
 go   
 create schema gold;
+go
